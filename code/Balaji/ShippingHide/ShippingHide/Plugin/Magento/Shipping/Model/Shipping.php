0f86a99f77f2c802cdfc8497188272dcade4c563
balaji <balajimrv@gmail.com>
2022-05-16 07:41:39 +0530

    shipping

---
+++ code/Balaji/ShippingHide/ShippingHide/Plugin/Magento/Shipping/Model/Shipping.php
@@ -0,0 +1,50 @@
+<?php
+namespace Balaji\ShippingHide\Plugin\Magento\Shipping\Model;
+ 
+class Shipping
+{
+    protected $product;
+ 
+    public function __construct(
+        \Magento\Catalog\Model\ProductFactory $product
+    ) {
+        $this->product = $product; 
+    }
+ 
+    public function aroundCollectCarrierRates(
+        \Magento\Shipping\Model\Shipping $subject,
+        \Closure $proceed,
+        $carrierCode,
+        $request
+    ) {
+        $samedaydelivery = false;
+        $allItems = $request->getAllItems();
+         
+        // iterate all cart products to check if samedaydelivery_attribute is true
+        
+        print_r(($allItems);
+        exit;
+        foreach ($allItems as $item) {    
+            $_product = $this->product->create()->load($item->getProduct()->getId());
+            // if product has samedaydelivery_attribute true
+            if ($_product->getSamedaydeliveryAttribute()) {
+                $samedaydelivery = true;
+                break;
+            }
+        }
+        
+       /* 
+        if ($samedaydelivery && $carrierCode == 'freeshipping') {
+            return false;
+        }
+         */
+         
+          if ($samedaydelivery) {
+            return false;
+        }
+         
+ 
+        $result = $proceed($carrierCode, $request);
+        return $result;
+    }
+}
